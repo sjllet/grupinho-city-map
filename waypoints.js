160d735daee39689c3e397edd0f05f57f5da1170
sjllet <cordilleramapper@gmail.com>
2025-04-17 20:27:26 -0400

    Add files via upload

---
+++ waypoints.js
@@ -0,0 +1,102 @@
+const waypoints = [
+	{
+		name: "La Gloriosa",
+		x: 8,
+		z: -278
+	},
+	{
+		name: "Puente Cocapol de la Enemistad",
+		x: 98,
+		z: 11
+	},
+	{
+		name: "Puente San Jairo Concha",
+		x: 8,
+		z: -126
+	},
+	{
+		name: "Torre Pija",
+		x: -412,
+		z: -262
+	},
+	{
+		name: "Void Tower",
+		x: -25,
+		z: -514
+	},
+	{
+		name: "Burj Khalifa",
+		x: 247,
+		z: -407
+	},
+	{
+		name: "Ueno Judío Bank",
+		x: 77,
+		z: -389
+	},
+	{
+		name: "Estadio Bancopol Mcal. Alfonso",
+		x: -264,
+		z: -41
+	},
+	{
+		name: "Stanislav Arena",
+		x: 554,
+		z: -376
+	},
+	{
+		name: "Estadio SC Motín",
+		x: 135,
+		z: 320
+	},
+	{
+		name: "Plaza Memorial Mariano",
+		x: 248,
+		z: 38
+	},
+	{
+		name: "La Palangana Sur",
+		x: -213,
+		z: -254
+	},
+	{
+		name: "Estadio Atlético Tuerca",
+		x: -127,
+		z: -474
+	},
+	{
+		name: "Central Nuclear Cocacinia",
+		x: 167,
+		z: 140
+	},
+	{
+		name: "World Trade Center",
+		x: -150,
+		z: -35
+	},
+	{
+		name: "Bancopol S. A.",
+		x: -297,
+		z: 30
+	},
+	{
+		name: "Grupiwood Studios",
+		x: -229,
+		z: 45
+	},
+	{
+		name: "IKEA Silipol",
+		x: -92,
+		z: -23
+	},
+	{
+		name: "Grupinho Ristorante",
+		x: -98,
+		z: 34
+	},
+	{
+		name: "Don Eladio (Mcal. Alfonso)",
+		x: -25,
+		z: 53
+	}
+];
